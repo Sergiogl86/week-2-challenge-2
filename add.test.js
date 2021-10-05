8a3de0e7be4a4efca69f58150c66713b5b84bb0b
Sergio Garcia <sergio.gl86@gmail.com>
2021-10-05 17:53:40 +0200

    Haciendo las funciones no excepciones

--- add.test.js
+++ add.test.js
@@ -1,5 +1,72 @@
 const { result } = require("lodash");
 
 function strictEquals(a, b) {
-  return result;
+  return Object.is(a, b);
 }
+
+describe("function compare", function () {
+  test("when function add receives 1 and 1, it returns true", function () {
+    //AAA -> Arrange  Act  Assert
+
+    //Arrange
+    const n1 = 1;
+    const n2 = 1;
+    const expected = true;
+
+    //Act
+
+    const result = strictEquals(n1, n2);
+
+    //Assert
+
+    expect(result).toBe(expected);
+  });
+  test("when function add receives 1 and '1', it returns false", function () {
+    //AAA -> Arrange  Act  Assert
+
+    //Arrange
+    const n1 = 1;
+    const n2 = "1";
+    const expected = false;
+
+    //Act
+
+    const result = strictEquals(n1, n2);
+
+    //Assert
+
+    expect(result).toBe(expected);
+  });
+  test("when function add receives true and false, it returns false", function () {
+    //AAA -> Arrange  Act  Assert
+
+    //Arrange
+    const n1 = true;
+    const n2 = false;
+    const expected = false;
+
+    //Act
+
+    const result = strictEquals(n1, n2);
+
+    //Assert
+
+    expect(result).toBe(expected);
+  });
+  test("when function add receives false and false, it returns true", function () {
+    //AAA -> Arrange  Act  Assert
+
+    //Arrange
+    const n1 = false;
+    const n2 = false;
+    const expected = true;
+
+    //Act
+
+    const result = strictEquals(n1, n2);
+
+    //Assert
+
+    expect(result).toBe(expected);
+  });
+});
